93d6212da870f68f58bf87d7250066517f65c22d
wwmm2021 <80555274+wwmm2021@users.noreply.github.com>
2021-04-19 17:00:35 -0400

    maximizing a normal likelihood
    model: maximizing a normal likelihood

---
+++ maximizing a normal likelihood.R
@@ -0,0 +1,13 @@
+make.NegLogLik <- function(data,fixed = c(FALSE,FALSE)){
+        params <- fixed
+        function(p){
+          parames[!fixed] <-p
+          mu <- params[1]
+          sigma <- params[2]
+          a <- -0.5*length(data)*log(2*pi*sigma^2)
+          b <- -0.5*sum((data-mu)^2)/(sigma^2)
+          -(a+b)
+        }
+  
+  
+}
